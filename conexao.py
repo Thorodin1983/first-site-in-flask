3922845cb86f0f9a36018c96052637302766c66f
Luan Vasconcelos Viana <118625580+Thorodin1983@users.noreply.github.com>
2024-09-30 10:09:59 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -4,10 +4,10 @@ import pyodbc
 def create_connection():
     try:
         conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                              'Server=SERVDB4\\SQLEXPRESS;'
-                              'Database=RA;'
-                              'UID=sa;'
-                              'PWD=Luan@1983;')
+                              'Server=<servidor>;'
+                              'Database=<database>;'
+                              'UID=<user>;'
+                              'PWD=<password>;')
         print("Conexão bem-sucedida!")
         return conn
     except Exception as e:
